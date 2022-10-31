fbb74e4b18de05d2d3fe43ff975338104d34e43f
Vitor <caulicons@hotmail.com>
2022-10-31 16:08:54 -0300

    fist commit

---
+++ pages/aboutScreen/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function AboutScreen() {
+
+  return (
+    <>
+      <h1>
+        this is AboutScreen
+      </h1>
+      <p>Hello word</p>
+      <Link href={"/"}>
+        Home Screen
+      </Link>
+    </>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function HomeScreen() {
+
+  return (
+    <>
+      <h1>
+        this is homePage
+      </h1>
+      <p>Hello word</p>
+      <Link href={"/aboutScreen"}>
+        About Screen
+      </Link>
+    </>
+  )
+}
