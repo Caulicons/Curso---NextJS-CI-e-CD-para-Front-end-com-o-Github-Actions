a81baacd7e8343e981e2d3e1efd395086e2b629d
Vitor <caulicons@hotmail.com>
2022-11-01 10:15:54 -0300

    setting up: Preview mode 05

--- scripts/previewCI.js
+++ scripts/previewCI.js
@@ -4,4 +4,4 @@ console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
 console.log(output);
-console.log("[DEPLOY_PREVIEW]: START");
+console.log("[DEPLOY_PREVIEW]: END");
